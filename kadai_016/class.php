d4ab2a762f507aeab899aa7858d402e7940e4331
rino821 <matsurino@outlook.jp>
2024-11-21 13:33:48 +0900

    ファイルをアップロード

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -8,39 +8,55 @@
 <body>
   <p>
     <?php
+    //クラスの定義
     class Food {
-
-      private $name;
-      private $price;
-
+      //プロパティの定義
+      public $name;
+      public $price;
+      //コンストラクタを定義
       public function __construct(string $name, int $price) {
         $this->name = $name;
         $this->price = $price;
       }
+      //メソッドを定義
+      public function show_price() {
+        echo $this->price;
+      }
     }
-
-    $food = new Food('potato', 250);
-
+    //インスタンス化する
+    $food = new Food('potato',250);
+    //値を出力する
     print_r($food);
+    echo '<br>';
 
-
+    //クラスの定義
     class Animal {
-
-      private $name;
-      private $height;
-      private $weight;
-
+      //プロパティの定義
+      public $name;
+      public $height;
+      public $weight;
+      //コンストラクタを定義
       public function __construct(string $name, int $height, int $weight) {
         $this->name = $name;
         $this->height = $height;
         $this->weight = $weight;
       }
+      //メソッドを定義
+      public function show_height() {
+        echo $this->height;
+      }
     }
-
-    $animal = new Animal('dog', 60, 5000);
-
+    //インスタンス化する
+    $animal = new Animal('dog',60,5000);
+    //値を出力する
     print_r($animal);
+    echo '<br>';
+    //メソッドを出力
+    $food->show_price();
+    echo '<br>';
+    //メソッドを出力
+    $animal->show_height();
     ?>
-  </p>  
+  </p>
 </body>
 </html>
